5ad835ca936126caec4e0ff9b29ecac0c9ab0389
SShreyaskunte <shreyas.kunte@tietoevry.com>
2022-09-22 15:59:50 +0530

    Add project files.

---
+++ TE_CSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System; //Namespaces - collection of classes, in clasess we have collection of methods
+
+//user defined namespace
+namespace Basic_Concepts
+{
+
+    /// <summary>
+    /// Basic C-Sharp concepts
+    /// </summary>
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to C-Sharp");
+        } 
+    }
+}
